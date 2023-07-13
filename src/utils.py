c9406e1b41ac9c6fb929c226d2875f8f5602594c
Paurnima Chavan <paurnimach@gmail.com>
2023-07-13 10:55:47 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,8 @@ import numpy as np
 
 
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    img = img / 2 + 0.5     # unnormalize    
+    plt.imshow(np.transpose(img, (1, 2, 0)))
     plt.show()
 
 
